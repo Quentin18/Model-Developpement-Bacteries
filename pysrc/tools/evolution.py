c03e92ce16678d5a6576d66fd1a432c2abb2efda
Quentin18 <quentindeschamps18@orange.fr>
2020-04-28 12:20:45 +0200

    Modif labels evolution

--- pysrc/tools/evolution.py
+++ pysrc/tools/evolution.py
@@ -8,7 +8,8 @@ from scipy.integrate import odeint
 
 class Evolution:
     """Gestion de l'évolution d’un système autonome"""
-    def __init__(self, title="Evolution", figsize=(10, 6)):
+    def __init__(self, title="Evolution du modèle en fonction du temps",
+                 figsize=(10, 6)):
         self._title = title
         self._figsize = figsize
 
@@ -51,11 +52,12 @@ class Evolution:
             if superpose:   # Cas superposition
                 for i in range(trj.shape[1]):
                     axes[0].plot(tdisc, trj[:, i], cnd.get_style(),
-                                 label=str(cnd0))
+                                 label=modl.str_cndzr() + str(cnd0))
             else:           # Cas graphiques séparés
                 i = 0
                 for ax in axes:
-                    ax.plot(tdisc, trj[:, i], cnd.get_style(), label=str(cnd0))
+                    ax.plot(tdisc, trj[:, i], cnd.get_style(),
+                            label=modl.str_cndzr() + str(cnd0))
                     i += 1
 
         # Paramétrages axes
@@ -67,16 +69,18 @@ class Evolution:
             ax.legend()
             if superpose:
                 ax.set_title(self.title)
-                ax.set(xlabel="Temps", ylabel=", ".join(modl.labels))
+                ax.set(xlabel="Temps", ylabel=", ".join(modl.symb))
             else:
                 ax.set_title(f"{modl.labels[i]} en fonction du temps")
-                ax.set(xlabel="Temps", ylabel=modl.labels[i])
+                ax.set(xlabel="Temps", ylabel=modl.symb[i])
             i += 1
 
         plt.tight_layout()
         plt.show()
 
         if exprtpng:
-            figname = modl.title + ".png"
-            figname = figname.replace(" ", "_")
+            if superpose:
+                figname = "evol_model_superpose.png"
+            else:
+                figname = "evol_model_separe.png"
             fig.savefig("img/" + figname)
